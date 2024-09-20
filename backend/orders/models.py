4626d01cfae604972376b9d288ab8afb946f5dda
michael <michaelngige@gmail.com>
2024-09-20 11:20:38 +0300

    project set up

---
+++ backend/orders/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Customer(models.Model):
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=10, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    item = models.CharField(max_length=100)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    order_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.item
