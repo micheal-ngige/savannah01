3c9a82b8bcf508c500a968636e604058bdfeadff
michael <michaelngige@gmail.com>
2024-09-20 11:29:45 +0300

    adding serializers,views,urls files

--- backend/orders/views.py
+++ backend/orders/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Customer, Order
+from .serializers import CustomerSerializer, OrderSerializer
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+class CustomerViewSet(viewsets.ModelViewSet):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+    permission_classes = [IsAuthenticated]
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+    permission_classes = [IsAuthenticated]
