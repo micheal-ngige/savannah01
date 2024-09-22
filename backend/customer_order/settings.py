8f74ddd9481286a32a57f28f84c93826d74b7698
michael <michaelngige@gmail.com>
2024-09-22 14:07:39 +0300

    updating app.yml secret key

--- backend/customer_order/settings.py
+++ backend/customer_order/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a$fzkttlx8umofcdgcf+@hsume1%)-dqtih6+1-oa495(n405*'
+SECRET_KEY = os.getenv('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
